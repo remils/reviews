d1a5cc10728634509803fdeadb577a83be931399
Sergey Zatulivetrov <remils@mail.ru>
2022-04-05 14:07:41 +0300

    маршруты и контролер отзывов

---
+++ app/Http/Controllers/Api/ReviewController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Response;
+
+class ReviewController extends Controller
+{
+    public function list()
+    {
+        return Response::json();
+    }
+
+    public function create()
+    {
+        return Response::json();
+    }
+}
